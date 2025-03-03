8670f452d55d92d6adf1e3871ed708001fa31f60
Frenadol <fernandoramajimenez@gmail.com>
2025-03-03 17:32:24 +0100

    primera subida de archivos

---
+++ src/main/java/com/frenadol/diccionariofernandospringboot/DiccionarioFernandoSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.frenadol.diccionariofernandospringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiccionarioFernandoSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiccionarioFernandoSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/frenadol/diccionariofernandospringboot/DiccionarioFernandoSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frenadol.diccionariofernandospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiccionarioFernandoSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
